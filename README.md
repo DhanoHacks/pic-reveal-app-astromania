75e82195cb5c4b2aaf68f2a6aceffe844c2737c4
DhanoHacks <draman9224@gmail.com>
2023-01-03 13:30:11 +0530

    Added basic functionality and README

--- README.md
+++ README.md
@@ -1,10 +1,2 @@
-# DhanoHacks-TicTacToe-for-Game-Dev-Hackathon
-This is the game submission for the game dev hackathon by team MicroHard™
-It is a simple game of tic-tac-toe, and you can start by clicking on the start game button
-you will be playing against a bot, and 
-After starting the game, the same "Start Game" button will change and can then be used to reset the game.
-
-A hosted version of the game is on https://dhanohacks.github.io/
-
-This was a good way to introduce ourselves to JavaScript, HTML and CSS. TicTacToe may sound like an easy project, but the logic involved
-in coding the bot so as to never lose to an opponent added a lot of lines to the script.
+# pic-reveal-app
+Hyperparameters: `grid_x` and `grid_y` in `main.js`
--- main.js
+++ main.js
@@ -1,291 +1,98 @@
+//initializing the game elements
+canvas=document.getElementById("game");
+var grid_x = 6;
+var grid_y = 4;
+canvas.width = 150*grid_x;
+canvas.height = 150*grid_y;
+const myImage = new Image(canvas.width, canvas.height);
+myImage.src = 'horsehead.png';
+let canvasElem = document.querySelector("canvas");
+var click_coords={x_coord:0,y_coord:0};
+var mouse_click_count=0;
+var board=[];
+for (let i = 0; i < grid_y; i++) { //making board
+    var row=[];
+    for (let j = 0; j < grid_x; j++) { 
+        row[j] = 1;
+    }
+    board[i] = row;
+}
+
 function resetBoard(){
+    canvas.style.display = "block";
     const context = canvas.getContext('2d');
     context.clearRect(0, 0, canvas.width, canvas.height);
+    board=[];
+    for (let i = 0; i < grid_y; i++) { //making board
+        var row=[];
+        for (let j = 0; j < grid_x; j++) { 
+            row[j] = 1;
+        }
+        board[i] = row;
+    }
+    click_coords={x_coord:0,y_coord:0};
     drawBoard();
-    var row1=[0,0,0];
-    var row2=[0,0,0];
-    var row3=[0,0,0];
-    board=[row1,row2,row3];
     mouse_click_count=0;
     var button = document.getElementById("start");
     if (button.value=="Start Game!") button.value = "Reset?";
-    document.getElementById("text").innerHTML="";
 }
 
-function drawBoard()
+function drawGrid()
   {
     var canvas = document.getElementById('game');
     if (canvas.getContext){
-        var ctx = canvas.getContext('2d'); 
-        var R = canvas.width / 6 * 0.75;
-        for (let i = 0; i < 2; i++) { //making board
+        var ctx = canvas.getContext('2d');
+        ctx.drawImage(myImage,0,0);
+        var R = canvas.width / (2 * grid_x) * 0.75;
+        for (let i = 0; i < grid_x - 1; i++) { //making board
             ctx.beginPath();
-            ctx.moveTo(canvas.width * (i+1) / 3, 1);
-            ctx.lineTo(canvas.width * (i+1) / 3, canvas.height-1);
+            ctx.moveTo(canvas.width * (i+1) / grid_x, 1);
+            ctx.lineTo(canvas.width * (i+1) / grid_x, canvas.height-1);
             ctx.strokeStyle = '#000000'; //black
             ctx.stroke();
         }
-        for (let i = 0; i < 2; i++) { //making board pt. 2
+        for (let i = 0; i < grid_y - 1; i++) { //making board pt. 2
             ctx.beginPath();
-            ctx.moveTo(1, canvas.height * (i+1) / 3);
-            ctx.lineTo(canvas.width-1, canvas.height * (i+1) / 3);
+            ctx.moveTo(1, canvas.height * (i+1) / grid_y);
+            ctx.lineTo(canvas.width-1, canvas.height * (i+1) / grid_y);
             ctx.strokeStyle = '#000000'; //black
             ctx.stroke();
         }
     }
 }
 
-function drawSymbol(mouse_click_count)
+function drawBoard(mouse_click_count)
 {
+    drawGrid();
     var canvas = document.getElementById('game');
-    var ctx = canvas.getContext('2d'); 
-    for (let i = 0; i < 3; i++) {
-        if(click_coords.x_coord>canvas.width*i/3 && click_coords.x_coord<canvas.width*(i+1)/3){
-            for (let j = 0; j < 3; j++) {
-                if(click_coords.y_coord>canvas.height*j/3 && click_coords.y_coord<canvas.height*(j+1)/3){
-                    if(board[j][i]==0){
-                        if(mouse_click_count%2==0){  //circle
-                            drawCircle(i,j);
-                        }
-                        else{     //cross
-                            drawX(i,j);
-                        }
-                        return 1;
-                    }
+    for (let i = 0; i < grid_x; i++) {
+        for (let j = 0; j < grid_y; j++) {
+            if(click_coords.y_coord>canvas.height*j/grid_y && click_coords.y_coord<canvas.height*(j+1)/grid_y && click_coords.x_coord>canvas.width*i/grid_x && click_coords.x_coord<canvas.width*(i+1)/grid_x){
+                if(board[j][i] == 0){
+                    board[j][i] = 1;
                 }
+                else if(board[j][i] == 1){
+                    board[j][i] = 0;
+                }
+            }
+            if(board[j][i] == 1){
+                coverBox(i,j);
             }
         }
     }
     return 0;
 }
 
-function drawCircle(i,j){
+function coverBox(i,j){
     var canvas = document.getElementById('game');
     var ctx = canvas.getContext('2d');
-    board[j][i]=1;
-    var R = canvas.width / 6 * 0.75;
-    var X = canvas.width * i / 3 + canvas.width / 6;
-    var Y = canvas.height * j / 3 + canvas.height / 6;
-    ctx.beginPath();
-    ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
-    ctx.lineWidth = 3;
-    ctx.strokeStyle = '#FF0000'; //red
-    ctx.stroke();
+    var boxWidth = canvas.width / grid_x - 2;
+    var X = canvas.width * i / grid_x + 1;
+    var Y = canvas.height * j / grid_y + 1;
+    ctx.fillStyle = '#FFFFFF'; //red
+    ctx.fillRect(X, Y, boxWidth, boxWidth);
 }
 
-function drawX(i,j){
-    var canvas = document.getElementById('game');
-    var ctx = canvas.getContext('2d');
-    board[j][i]=-1;
-    ctx.beginPath();
-    ctx.moveTo(canvas.width * (i+0.2) / 3, canvas.height * (j+0.2) / 3);
-    ctx.lineTo(canvas.width * (i+0.8) / 3, canvas.height * (j+0.8) / 3);
-    ctx.strokeStyle = '#0000FF'; //blue
-    ctx.stroke();
-    ctx.moveTo(canvas.width * (i+0.2) / 3, canvas.height * (j+0.8) / 3);
-    ctx.lineTo(canvas.width * (i+0.8) / 3, canvas.height * (j+0.2) / 3);
-    ctx.strokeStyle = '#0000FF'; //blue
-    ctx.stroke();
-}
-
-function gameLogic(board,mouse_click_count){
-    if (checkWinHuman(board)){
-        document.getElementById("text").innerHTML="wow you actually won";
-        return 0;
-    }
-    var count=0;
-    var turn_no=1;
-    for(let i=0; i<3;i++){
-        for(let j=0;j<3;j++){
-            if(board[j][i]!=0){
-                turn_no++;
-            }
-        }
-    }
-    if(turn_no==10){
-        document.getElementById("text").innerHTML="its a tie!";
-        return 0;
-    }
-    if(board[1][1]==0&&count==0&&turn_no==2){
-        drawX(1,1);
-        count=1;
-    }
-    else if(board[0][0]==0&&count==0&&turn_no==2){
-        drawX(0,0);
-        count=1;
-    }
-    for(let i=0; i<3;i++){     //X win condition
-        for(let j=0;j<3;j++){
-            if(checkWinCondition(board,i,j)!=9999){
-                if(checkWinCondition(board,i,j)<0&&count==0){
-                    drawX(i,j);
-                    count=1;
-                }
-            }
-        }
-    }
-    for(let i=0; i<3;i++){      //O win condition
-        for(let j=0;j<3;j++){
-            if(checkWinCondition(board,i,j)!=9999){
-                if(checkWinCondition(board,i,j)>0&&count==0){
-                    drawX(i,j);
-                    count=1;
-                }
-            }
-        }
-    }
-    count=checkDoubleTrick(count,board);
-    if(count==0){
-        for(let i=0; i<3;i++){
-            for(let j=0;j<3;j++){
-                if (checkWinCondition(board,i,j)!=9999&&count==0){
-                    var row1=[0,0,0];
-                    var row2=[0,0,0];
-                    var row3=[0,0,0];
-                    var board1=[row1,row2,row3];
-                    for(let i=0; i<3;i++){
-                        for(let j=0;j<3;j++){
-                            board1[j][i]=board[j][i];
-                        }
-                    }
-                    board1[j][i]=-1;
-                    var count2=0;
-                    for(let i1=0; i1<3;i1++){
-                        for(let j1=0;j1<3;j1++){
-                            if(checkWinCondition(board1,i1,j1)==-1&&count2==0){
-                                board1[j1][i1]=1;
-                                count2=1;
-                            }
-                        }
-                    }
-                    if(count2==0){
-                        var count2=0;
-                    for(let i1=0; i1<3;i1++){
-                        for(let j1=0;j1<3;j1++){
-                            if(checkWinCondition(board1,i1,j1)==-1&&count2==0){
-                                board1[j1][i1]=1;
-                                count2=1;
-                            }
-                        }
-                    }
-                    }
-                    var count3=0;
-                    for(let i1=0; i1<3;i1++){
-                        for(let j1=0;j1<3;j1++){
-                            if(checkWinCondition(board1,i1,j1)==1){
-                                count3++;
-                            }
-                        }
-                    }
-                    if(count3<2){
-                        drawX(i,j);
-                        count=1;
-                    }
-                }
-            }
-        }
-    }
-    if (checkWinBot(board)){
-        document.getElementById("text").innerHTML="you lost hahahaha";
-        return 0;
-    }
-}
-
-function checkWinCondition(board,i,j){
-    if (board[j][i]==0){
-        var x=board[(j+1)%3][i];
-        if(x!=0&&board[(j+1)%3][i]==x&&board[(j+2)%3][i]==x){
-            return x;
-        }
-        var y=board[j][(i+1)%3];
-        if(y!=0&&board[j][(i+1)%3]==y&&board[j][(i+2)%3]==y){
-            return y;
-        }
-        var z=board[(j+1)%3][(i+1)%3];
-        if(z!=0&&i==j&&board[(j+1)%3][(i+1)%3]==z&&board[(j+2)%3][(i+2)%3]==z){
-            return z;
-        }
-        var w=board[(j+2)%3][(i+1)%3];
-        if(w!=0&&i+j==2&&board[(j+2)%3][(i+1)%3]==w&&board[(j+1)%3][(i+2)%3]==w){
-            return w;
-        }
-        return 0;
-
-    }
-    else return 9999;
-}
-
-function checkWinBot(board){
-    for(let i=0; i<3;i++){
-        for(let j=0;j<3;j++){
-            if (board[j][i]==-1){
-                var x=board[(j+1)%3][i];
-                if(x==-1&&board[(j+1)%3][i]==x&&board[(j+2)%3][i]==x){
-                    return 1;
-                }
-                var y=board[j][(i+1)%3];
-                if(y==-1&&board[j][(i+1)%3]==y&&board[j][(i+2)%3]==y){
-                    return 1;
-                }
-                var z=board[(j+1)%3][(i+1)%3];
-                if(z==-1&&i==j&&board[(j+1)%3][(i+1)%3]==z&&board[(j+2)%3][(i+2)%3]==z){
-                    return 1;
-                }
-                var w=board[(j+2)%3][(i+1)%3];
-                if(w==-1&&i+j==2&&board[(j+2)%3][(i+1)%3]==w&&board[(j+1)%3][(i+2)%3]==w){
-                    return 1;
-                }
-            }
-        }
-    }
-    return 0;
-}
-
-function checkWinHuman(board){
-    for(let i=0; i<3;i++){
-        for(let j=0;j<3;j++){
-            if (board[j][i]==1){
-                var x=board[(j+1)%3][i];
-                if(x==1&&board[(j+1)%3][i]==x&&board[(j+2)%3][i]==x){
-                    return 1;
-                }
-                var y=board[j][(i+1)%3];
-                if(y==1&&board[j][(i+1)%3]==y&&board[j][(i+2)%3]==y){
-                    return 1;
-                }
-                var z=board[(j+1)%3][(i+1)%3];
-                if(z==1&&i==j&&board[(j+1)%3][(i+1)%3]==z&&board[(j+2)%3][(i+2)%3]==z){
-                    return 1;
-                }
-                var w=board[(j+2)%3][(i+1)%3];
-                if(w==1&&i+j==2&&board[(j+2)%3][(i+1)%3]==w&&board[(j+1)%3][(i+2)%3]==w){
-                    return 1;
-                }
-            }
-        }
-    }
-    return 0;
-}
-
-function checkDoubleTrick(count,board){
-    if(count==0&&board[0][0]==0&&board[0][1]==1&&board[1][0]==1&&board[0][2]==0&&board[2][0]==0){
-        drawX(0,0);
-        return 1;
-    }
-    if(count==0&&board[0][2]==0&&board[0][1]==1&&board[1][2]==1&&board[0][0]==0&&board[2][2]==0){
-        drawX(2,0);
-        return 1;
-    }
-    if(count==0&&board[2][0]==0&&board[2][1]==1&&board[1][0]==1&&board[0][0]==0&&board[2][2]==0){
-        drawX(0,2);
-        return 1;
-    }
-    if(count==0&&board[2][2]==0&&board[2][1]==1&&board[1][2]==1&&board[0][2]==0&&board[2][0]==0){
-        drawX(2,2);
-        return 1;
-    }
-    return count;
-}
 
 function getMousePosition(canvas, event, click_coords) {
     let rect = canvas.getBoundingClientRect();
@@ -295,34 +102,20 @@ function getMousePosition(canvas, event, click_coords) {
     click_coords.y_coord=y;
 }
 
-//initializing the game elements
-canvas=document.getElementById("game");
-canvas.width = 600;
-canvas.height = 600;
-let canvasElem = document.querySelector("canvas");
-var click_coords={x_coord:0,y_coord:0};
-var mouse_click_count=0;
-var row1=[0,0,0];
-var row2=[0,0,0];
-var row3=[0,0,0];
-var board=[row1,row2,row3];
-
 canvasElem.addEventListener("mousedown", function(e)
 {
     var cnt=0;
-    for(let i=0; i<3;i++){
-        for(let j=0;j<3;j++){
-            if (board[j][i]!=0){
+    for(let i=0; i<grid_x;i++){
+        for(let j=0;j<grid_y;j++){
+            if (board[j][i]!=1){
                 cnt++;
             }
         }
     }
-    if(document.getElementById("start").value!="Start Game!"&&cnt!=9)
+    if(document.getElementById("start").value!="Start Game!"&&cnt!=grid_x*grid_y)
         getMousePosition(canvasElem, e, click_coords);
-        var result=drawSymbol(mouse_click_count);
+        var result=drawBoard(mouse_click_count);
         if(result==1){
-            mouse_click_count++;
-            gameLogic(board,mouse_click_count);
             mouse_click_count++;
         }
 });
